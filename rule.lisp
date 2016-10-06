e916c4b9fb42fc7d5fd8e9b023b4c40cfd1a6277
Diogo Franco <diogoalexfranco@gmail.com>
2016-10-06 17:35:52 +0100

    add rule.lisp file with structure definitions

---
+++ rule.lisp
@@ -0,0 +1,23 @@
+;;
+;; This file contains the code for defining
+;; the rule structure.
+;;
+
+(in-package :cl-association-rules)
+
+(defstruct (rule
+            (:print-function
+              (lambda (struct stream depth)
+                (declare (ignore depth))
+                (if (or (rule-pretuple struct)
+                        (rule-posttuple struct)
+                        (rule-support struct)
+                        (rule-confidence struct))
+                  (format stream "~a => ~a. Support is ~a and confidence is ~a."
+                          (rule-pretuple struct)
+                          (rule-posttuple struct)
+                          (rule-support struct)
+                          (rule-confidence struct))
+                  (format stream "New empty rule.")))))
+
+  pretuple posttuple support confidence)
